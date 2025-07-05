495fdfce77b36b032716768a46ff236060173bca
IreneTayler <Irene19tayler@outlook.com>
2025-07-05 17:46:24 +0300

    Finished Medical history

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import authRoute from "./routes/auth"
 import loginRoute from './routes/login'
 import emergencyRoutes from './routes/emergency';
+import medicalRoutes from './routes/medical';
 dotenv.config();
 
 const app = express();
@@ -38,6 +39,7 @@ app.get('/', (_req, res) => {
 app.use('/api/auth', authRoute)
 app.use('/api/login',loginRoute)
 app.use('/api/emergency', emergencyRoutes);
+app.use('/api/medical', medicalRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
